04e2ff38bedf6b6fafc878d4ba2c7a9e6e9de035
root <juanandreskoch@gmail.com>
2025-03-25 00:37:36 -0400

    Creacion de modal para registro de tiendas, funcion para registro de tiendas y funcion para mostrar las tiendas registradas en la base de datos

--- mall_app/blueprints/admin/routes.py
+++ mall_app/blueprints/admin/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, redirect, url_for, Blueprint
+from flask import request, render_template, redirect, url_for, Blueprint, current_app
 from flask_login import login_user, logout_user, current_user
 from flask_bcrypt import Bcrypt, generate_password_hash
 
@@ -11,5 +11,50 @@ def index():
 
 @admin.route('/tiendas', methods = ['GET', 'POST'])
 def tiendas():
+    db = current_app.config['db']
+
     if request.method == 'GET':
-        return render_template('admin/tiendas.html')
+        cur = db.cursor()
+
+        cur.execute('SELECT t.idtiendas, t.nombre_tienda, u.nombre, u.apellido FROM tiendas t JOIN usuarios u ON t.usuarios = u.idusuarios')
+        data = cur.fetchall()
+        cur.close()
+        insertTienda = []
+        columNamnes = [column[0] for column in cur.description]
+        for record in data:
+            insertTienda.append(dict(zip(columNamnes, record)))
+        
+        print(insertTienda)
+        return render_template('admin/tiendas.html', tiendas = insertTienda)
+    
+    if request.method == 'POST':
+
+        nombreTienda = request.form['tienda']
+        nombreEncargado = request.form['nombre']
+        apellidoEncargado = request.form['apellido']
+        email = request.form['email']
+        contraseña = request.form['contraseña']
+        rol = 'tienda'
+        hash_contraseña = bcrypt.generate_password_hash(contraseña).decode('utf-8')
+
+        try:
+            cur = db.cursor()
+
+            sql = 'INSERT INTO usuarios (`nombre`, `apellido`, `email`, `contraseña`, `rol`) VALUES (%s, %s, %s, %s, %s)'
+            data = (nombreEncargado, apellidoEncargado, email, hash_contraseña, rol)
+            cur.execute(sql, data)
+            db.commit()
+
+            cur.execute('SELECT idusuarios FROM usuarios WHERE email =  %s', (email,))
+            idusuario = cur.fetchone()
+
+            sql_tienda = 'INSERT INTO tiendas (`usuarios`, `nombre_tienda`) VALUES (%s, %s)'
+            data_tienda = (idusuario, nombreTienda)
+            cur.execute(sql_tienda, data_tienda)
+            db.commit()
+
+        except Exception as e:
+            print(e)
+        finally:
+            cur.close()
+        return redirect(url_for('admin.tiendas'))
