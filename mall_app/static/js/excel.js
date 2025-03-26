6ef317ceb880d9508801af1ddcd0340ab2af9f62
root <juanandreskoch@gmail.com>
2025-03-26 16:37:36 -0400

    Mejorando funcion para migrar a excel

--- mall_app/static/js/excel.js
+++ mall_app/static/js/excel.js
@@ -1,11 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const btnExportar = document.getElementById('btn-excel'), 
         reportes = document.getElementById('tablaReportes');
     
         let tableExport = new TableExport(reportes, {
             exportButtons: false,
             filename: "ReportesMarzo",
-            sheetname: "Tabla"
+            sheetname: "Tabla",
+            ignoreColumns: [7, 8],
+            exportStyles: true // Asegura que se respeten los estilos CSS
         });
     btnExportar.addEventListener('click', () => {
         let datos = tableExport.getExportData();
@@ -20,5 +22,40 @@ document.addEventListener('DOMContentLoaded', function() {
             pref.sheetname
         );
 
+        window.location.href = '/admin/';
     })
-})
+})*/
+
+function excel() {
+    const reportes = document.getElementById('tablaReportes');
+    let tableExport = new TableExport(reportes, {
+        exportButtons: false,
+        filename: "ReportesMarzo",
+        sheetname: "Tabla",
+    });
+
+    let datos = tableExport.getExportData();
+    let pref = datos.tablaReportes.xlsx;
+    tableExport.export2file(
+            pref.data,
+            pref.mimetype,
+            pref.filename,
+            pref.fileExtension,
+            pref.merges,
+            pref.RTL,
+            pref.sheetname
+        );
+
+    window.location.href = '/admin/';
+}
+
+function migrarExcel() {
+
+    const btnsTabla = document.querySelectorAll('.td-btn')
+
+    for (let i = 0; i < btnsTabla.length; i++) {
+        btnsTabla[i].remove();
+    }
+
+    excel()
+}
